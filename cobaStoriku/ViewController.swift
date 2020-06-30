43ada289fa95c073a2028cd0edb447a6f39c00b7
Aji Suryawan <kazumovie132@gmail.com>
2020-06-30 22:50:16 +0700

    Initial Commit

---
+++ cobaStoriku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cobaStoriku
+//
+//  Created by Aji Suryawan on 30/06/20.
+//  Copyright © 2020 RPL RUS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
